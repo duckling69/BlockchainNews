b960fa04a7cc9a54921b8128b74b999cb1ea8748
Sumant.xD <st72365@gmail.com>
2023-01-07 00:39:06 +0530

    basic animation added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      backgroundImage: theme => ({
+         'stroke': "url('../assets/stroke.png')",
+      })
+
+    },
   },
   plugins: [],
 }
