2a5e7cf57ab61d099368673f9708622038996eb3
8rxn <rajaryanvishwakarma10@gmail.com>
2023-01-06 22:52:51 +0530

    landingpage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import image from './image.jpg'
+import heroImage from '../assets/hero.svg'
 
 
 const Component = () => {
     return (
         <div className=" font-mono px-4 py-2 mt-[12vh] flex items-center lg:flex-row flex-col-reverse">
             {/* Text and input */}
-            <div className="text-gray-500 leading-tight pl-[5vw] max-w-fit lg:ml-0 lg:mr-auto lg:mx-0 ">
-                <h1 className="text-5xl font-semibold"> Let the Truth Reach Everyone</h1>
+            <div className="text-gray-500 leading-tight pl-[5vw] w-2/5 lg:ml-0 lg:mr-auto lg:mx-0 ">
+                <h1 className="text-5xl font-semibold"> Let the Truth Reach <span className='hero-text'>Everyone</span></h1>
                 <p className="mt-2"> Post anonymous bits that are useful for the community. </p>
                 <div className="mt-6 lg:flex flex flex-col lg:justify-between">
                     <p className="mt-2"></p>
                     <div className='flex flex-row mt-2'>
-                        <div className="relative rounded-md shadow-sm mt-4 lg:mt-0">
-                            <input type="text" className="form-input py-3 px-4 block w-full leading-5 rounded-md transition duration-150 ease-in-out sm:text-sm sm:leading-5" placeholder="Enter Email" />
-                        </div>
+                        
                         {/* {"Icons for social"} */}
+                        <div className='text-center flex gap-5 w-full'>
+
+                        <button className='inline-block text-xl px-4 py-2 leading-none border bg-purple-500 text-white rounded-md   hover:bg-purple-700 mt-4 lg:mt-0 transition duration-150 ease-in-out'>
+                            Login
+                        </button>
+                        <button className='inline-block text-xl px-4 py-2 leading-none border bg-purple-500 text-white rounded-md   hover:bg-purple-700 mt-4 lg:mt-0 transition duration-150 ease-in-out'>
+                            Signup
+                        </button>
+                        </div>
                     </div>
 
                 </div>
             </div>
             {/* Image */}
-            <img src={image} alt="Image" className="h-full w-2/4 object-cover" />
+            <img src={heroImage} alt="Image" className="h-full w-fit object-cover" />
         </div>
     );
 };
