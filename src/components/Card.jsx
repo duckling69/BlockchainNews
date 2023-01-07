06e626bc314a7b2d388c76ac908e8178fb78b5db
shirsho-roy <shirsho.tffs@gmail.com>
2023-01-07 20:04:38 +0530

    changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {BiUpvote} from 'react-icons/bi'
 const Card = ({item}) => {
   return (
     <div>
@@ -22,15 +22,16 @@ const Card = ({item}) => {
             <div className="flex items-center justify-between w-full ml-auto">
               <div className="text-sm text-slate-400">Tue, Jul 20</div>
               <div className="flex items-center gap-x-1">
-                <svg
+                {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="w-5 h-5 text-yellow-500"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-                <span className="text-sm font-bold">4.9</span>
+                </svg> */}
+                <BiUpvote className="w-5 h-5 text-yellow-500"/>
+                <span className="text-sm font-bold">52</span>
               </div>
             </div>
           </div>
